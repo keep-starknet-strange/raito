7b6a958422b62ffe1f07655901d48d9847f7de0a
TAdev0 <angles.tristan@protonmail.com>
2024-08-12 16:27:47 +0200

    add block description in script

--- scripts/data/get_blocks.sh
+++ scripts/data/get_blocks.sh
@@ -10,7 +10,7 @@ get_single_block() {
 main() {
     local block_hashes=(
         "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"  # Genesis block
-        "00000000d1145790a8694403d4063f323d499e655c83426834d4ce2f8dd4a2ee"  # Block 170
+        "00000000d1145790a8694403d4063f323d499e655c83426834d4ce2f8dd4a2ee"  # Block containing first P2P tx to Hal Finney
     )
 
     # Loop through the block hashes and call get_block.sh for each
