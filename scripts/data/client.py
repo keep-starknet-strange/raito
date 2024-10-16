13b88f0cfae33b2304d86f724983118efac01fa8
raizo07 <agadaraizo@gmail.com>
2024-10-16 10:57:23 +0100

    fix: Isolate main logic into a separate function

--- scripts/data/client.py
+++ scripts/data/client.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
-
 import os
 import subprocess
 import random
 import argparse
 from pathlib import Path
-
 from generate_data import generate_data
 from format_args import format_args
 
 BASE_DIR = Path(".client_cache")
 DEFAULT_NO_OF_BLOCKS = 1
-
 
 def run_client(initial_height, num_blocks, mode):
     first = initial_height + 1
@@ -65,15 +62,27 @@ def run_client(initial_height, num_blocks, mode):
         print(result.stdout)
         return True
 
+def process_blocks(args):
+    BASE_DIR.mkdir(exist_ok=True)
+    end = args.start + args.blocks
+
+    for height in range(args.start, end, args.step):
+        if args.strategy == "sequential":
+            if not run_client(height, args.step, args.mode):
+                return
+        elif args.strategy == "random":
+            random_height = random.randint(0, 49)
+            if not run_client(random_height, 1, args.mode):
+                return
+        else:
+            print("Unsupported strategy")
+            return
 
 def main():
     parser = argparse.ArgumentParser(description="Run client script")
     parser.add_argument("--start", type=int, default=1, help="Start block height")
     parser.add_argument(
-        "--blocks",
-        type=int,
-        default=DEFAULT_NO_OF_BLOCKS,
-        help="Number of blocks to process",
+        "--blocks", type=int, default=DEFAULT_NO_OF_BLOCKS, help="Number of blocks to process"
     )
     parser.add_argument(
         "--step", type=int, default=1, help="Step size for block processing"
@@ -89,23 +98,7 @@ def main():
     )
 
     args = parser.parse_args()
-
-    BASE_DIR.mkdir(exist_ok=True)
-
-    end = args.start + args.blocks
-
-    for height in range(args.start, end, args.step):
-        if args.strategy == "sequential":
-            if not run_client(height, args.step, args.mode):
-                return
-        elif args.strategy == "random":
-            random_height = random.randint(0, 49)
-            if not run_client(random_height, 1, args.mode):
-                return
-        else:
-            print("Unsupported strategy")
-            return
-
+    process_blocks(args)
 
 if __name__ == "__main__":
     main()
